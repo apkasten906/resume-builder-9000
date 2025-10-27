d1a54a646c7ec0515bc16fc44e1095a68b79848c
apkasten906 <apkasten@gmail.com>
2025-10-27 22:19:50 +0100

    chore(commitlint): allow 'ci' commit type

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore']],
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'chore', 'ci']],
     'subject-case': [0, 'never'],
   },
 };
